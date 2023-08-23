3b554979e3935266ed09ae573c7ddb7863f45218
AnshumanDec2K <dalai.anshuman2000@gmail.com>
2023-08-23 22:10:44 +0530

    chat index file updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,19 @@
-function App() {
+import { useState } from "react";
+import { BrowserRoter, Routes, Route, Navigate } from "react-router-dom";
+import Chat from "@/components/chat";
 
+function App() {
   return (
     <>
       <div className="app">
-        app
+        <BrowserRoter>
+          <Routes>
+            <Route path="/chat" element={<Chat />} />
+          </Routes>
+        </BrowserRoter>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
